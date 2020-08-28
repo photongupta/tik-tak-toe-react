79cfb3f8e6f35a4ec5609dca413e8baea3fbd3ed
Rashmi Gupta <rashmi224141@gmail.com>
2020-08-28 14:48:48 +0530

    added first build and modified index.html

---
+++ build/precache-manifest.f825750db681d5a2ba7edee59d043156.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b80a20f0f0929c02c7240d870068a131",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c305f44f6863a2b81538",
+    "url": "/static/css/main.d07f3c01.chunk.css"
+  },
+  {
+    "revision": "bf62220405e16cd5594e",
+    "url": "/static/js/2.b71a303e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b71a303e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c305f44f6863a2b81538",
+    "url": "/static/js/main.4d8c7ec6.chunk.js"
+  },
+  {
+    "revision": "36b4b59da714df514934",
+    "url": "/static/js/runtime-main.93ad3f5b.js"
+  }
+]);
